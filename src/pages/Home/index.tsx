dce8453027b5c1723fa0a3966760f625ee913a5b
juniorHotes <juniorhotes53@gmail.com>
2021-02-13 23:30:58 -0300

    card user insert.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,21 @@
 import React, { ReactElement } from 'react';
 import Header from '../../components/Header'
+import CardUser from '../../components/CardUser'
+import { Link } from 'react-router-dom';
 
 function Home(): ReactElement {
+    const storage: any = localStorage.getItem("users")
+    const data = JSON.parse(storage)
+
     return (
         <>
             <Header title={"Pulse"} />
             <div className="container">
                 <h1>Página Home</h1>
+                {data.map((e: any) =>
+                    <Link key={e?.id} to={`/edit/user/${e?.id}`} >
+                        <CardUser key={e?.id} name={e?.name} email={e?.email} />
+                    </Link>)}
             </div>
         </>
     )
