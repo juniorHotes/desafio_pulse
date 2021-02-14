bb30a3c8fa86377b70e45e7599748d48a0ddcaa6
juniorHotes <juniorhotes53@gmail.com>
2021-02-13 23:25:35 -0300

    New route edit user.

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import RegisterUser from './pages/RegisterUser';
+import EditUser from './pages/EditUser';
 
 function Routes(): ReactElement {
   return (
@@ -11,6 +12,7 @@ function Routes(): ReactElement {
       <Route path="/" exact component={Home}/>
       <Route path="/register" component={RegisterUser}/>
       <Route path="/login" component={Login}/>
+      <Route path="/edit/user/:id" component={EditUser}/>
     </BrowserRouter>
   );
 }
